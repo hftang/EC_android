5d36674b5b7f36eefe86611e776f79229f8cf4c9
hftang <617775384@qq.com>
2018-09-04 09:02:28 +0800

    架构搭建完毕

---
+++ latte-core/src/main/java/com/hftang/latte/delegate/BaseDelegate.java
@@ -0,0 +1,46 @@
+package com.hftang.latte.delegate;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import butterknife.ButterKnife;
+import butterknife.Unbinder;
+import me.yokeyword.fragmentation_swipeback.SwipeBackFragment;
+
+/**
+ * 创建时间：2018/9/3
+ * 编写人：hftang
+ * 功能描述：
+ */
+public abstract class BaseDelegate extends SwipeBackFragment {
+
+    private Unbinder unbinder = null;
+
+
+    public abstract Object setlayout();
+
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+
+        View rootView = null;
+
+        if (setlayout() instanceof Integer) {
+            rootView = inflater.inflate((Integer) setlayout(), container, false);
+        } else if (setlayout() instanceof View) {
+            rootView = (View) setlayout();
+        }
+
+        if (rootView != null) {
+            unbinder = ButterKnife.bind(this, rootView);
+        }
+
+
+        return super.onCreateView(inflater, container, savedInstanceState);
+    }
+}
